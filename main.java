beb388de11494b076e67922968f12efdb5646a72
damien <damien@example.com>
2025-02-11 17:03:14 +0100

    message

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main
+{
+public static void main(String[] args) 
+{
+    Voiture voiture = new Voiture();
+    voiture.couleur = "Rouge";
+    voiture.marque = "Toyota";
+    voiture.modele = "corolla";
+    
+    voiture.demarrer();
+    
+}
+}
